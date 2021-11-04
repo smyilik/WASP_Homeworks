610962ff3050529355dec5ada4cd05b235284a16
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-04 08:48:16 +0300

    Add files via upload

---
+++ 2/2_Last_number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Academy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a % 10);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2/4_symmetric_number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Academy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            while (number > 0)
+            {
+                Console.Write(number % 10);
+                number /= 10;
+            }
+            Console.ReadKey();
+        }
+    }
+}
