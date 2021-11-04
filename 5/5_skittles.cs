6f3f1e109c9ea0a09c757fdc457526c849f6865c
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-04 10:03:12 +0300

    Add files via upload

---
+++ 5/5_skittles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] nk = Console.ReadLine().Split();//вводим кол-во кегель и бросков
+            int n = int.Parse(nk[0]), k = int.Parse(nk[1]);
+            string[] mass = new string[n];//создаём массив, который мы потом выведем
+            for (int i = 0; i < n; i++)//забиваем весь массив буквами "l"
+            {
+                mass[i] = "l";
+            }
+            string[] liri;
+            int li, ri;
+            for (int j = 0; j < k; j++)//вводим номера сбитых кегель
+            {
+                liri = Console.ReadLine().Split();
+                li = int.Parse(liri[0]);
+                ri = int.Parse(liri[1]);
+                for (int z = li - 1; z < ri; z++)//меняем "l" на ".", где это нужно
+                {
+                    mass[z] = ".";
+                }
+            }
+
+            foreach (string h in mass)
+            {
+                Console.Write(h);
+            }
+        }
+    }
+}
