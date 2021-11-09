bbfcc75aa89b1ca955772367d7c0ab4ce16670a8
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-09 17:32:30 +0300

    Add files via upload

---
+++ 7_Metro_1/WASP Lessons/Station.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace WASP_Lessons
+{
+    public class Station
+    {
+        public string name;
+        public string color;
+        public Line line;
+        public bool isWheelchairAccessible;
+        public bool hasParkAndRide;
+        public bool hasNearbyCableCar;
+        public List<Station> transfers;
+        public Station(string name, string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+        public Station(string name, string color, List<Station> transfers)
+        {
+            this.name = name;
+            this.color = color;
+            this.transfers = transfers;
+        }
+        public string GetName()
+        {
+            return (name);
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public bool IsWheelchairAccessible()
+        {
+            return isWheelchairAccessible;
+        }
+        public bool HasParkAndRide()
+        {
+            return hasParkAndRide;
+        }
+        public bool HasNearbyCableCar()
+        {
+            return hasNearbyCableCar;
+        }
+        public List<Station> GetTransferList()
+        {
+            return transfers;
+        }
+    }
+}
