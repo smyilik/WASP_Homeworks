b893994cb26afcab7430be20c672d6e4461bd017
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-09 17:30:37 +0300

    Add files via upload

---
+++ 7_Metro_1/WASP Lessons/Line.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace WASP_Lessons
+{
+    public class Line
+    {
+        public List<Station> stations;
+        public string name;
+        public string color;
+        public Line(string name, string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+        //public GetStation(string )
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public string GetColor()
+        {
+            return color;
+        }
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+        public void AddStation(string name, string color)
+        {
+            Station station = new Station(name, color);
+            stations.Add(station);
+        }
+        public void RemoveStation(string name)
+        {
+            Station station = new Station(name, color);
+            stations.Remove(station);
+        }
+        public Station FindStationByName(string name)
+        {
+            int len = stations.Count;
+            for(int i = 0; i < len; i++)
+            {
+                if (stations[i].name == name)
+                {
+                    return stations[i];
+                }
+            }
+        }
+        public List<Station> GetStationList(string name)
+        {
+            return stations;
+        }
+    }
+}
---
+++ 7_Metro_1/WASP Lessons/Metro.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace WASP_Lessons
+{
+    public class Metro
+    {
+        public List<Line> lines;
+        public string city;
+        public Metro(string city)
+        { 
+            this.city = city;
+        }
+        public string GetCity()
+        {
+            return city;
+        }
+        public void AddLine(string name, string color)
+        {
+            Line line = new Line(name, color);
+            lines.Add(line);
+        }
+        public void RemoveLine(string name)
+        {
+            int len = lines.Count;
+            for(int i = 0; i < len; i++)
+            {
+                if (lines[i].name == name)
+                {
+                    lines.Remove(lines[i]);
+                }
+            }
+        }
+        public List<Station> FindStation (string name)
+        {
+            int lineamount = lines.Count, stationamount;
+            Line line;
+            for (int i = 0; i < lineamount; i++)
+            {
+                line = lines[i];
+                stationamount = line.stations.Count;
+                for (int j = 0; j < stationamount; j++)
+                {
+                    if (line.stations[j].name == name)
+                    {
+                        return line.stations;
+                    }
+                }
+            }
+        }
+        public Station FindStation(string name, string lineName)
+        {
+            int lineamount = lines.Count, stationsamount;
+            Line line;
+            for (int i = 0; i < lineamount; i++)
+            {
+                line = lines[i];
+                stationsamount = line.stations.Count;
+                for(int j = 0; j < stationsamount; j++)
+                {
+                    if ((line.name == lineName) && (line.stations[j].name == name))
+                    {
+                        return line.stations[j];
+                    }
+                }
+            }
+        }
+        public List<Station> GetStationList(string name)
+        {
+            int lineamount = lines.Count, stationsamount;
+            Line line;
+            for (int i = 0; i < lineamount; i++)
+            {
+                line = lines[i];
+                stationsamount = line.stations.Count;
+                for(int j = 0; j < stationsamount; j++)
+                {
+                    if (line.stations[j].name == name)
+                    {
+                        return line.stations;
+                    }
+                }
+            }
+        }
+        /*public void LoadStationsFromFile(string filename)
+        {
+            StreamReader r = new StreamReader("C:\\Users\\sanya\\OneDrive\\Рабочий стол\\" + filename);
+            line.stations = r.ReadToEnd().Split('\n');
+        }*/
+    }
+}
