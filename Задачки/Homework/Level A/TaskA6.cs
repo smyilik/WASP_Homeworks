23c39b7506b15c34e7e000e87bc9fc61438b1074
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-10 20:56:05 +0300

    Add files via upload

---
+++ Задачки/Homework/Level A/TaskA6.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    // Задача А6.
+    // Кол-во стингеров: ⅓🔹
+    //
+    // Написать функцию OddNumbers, которая получает на вход список целых чисел arr и целое число n и
+    // возвращает список, состоящий из n последних вхождений нечетных чисел списка arr в том же порядке
+    //
+    // Пример:
+    // ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [5, 7, 9]
+    public static class TaskA6
+    {
+        public static List<int> OddNumbers(List<int> arr, int n)
+        {
+            List<int> ls = new List<int>();
+            for (int i = arr.Count - 1; i >= 0; i--)
+            {
+                if (arr[i] % 2 == 1) { ls.Add(arr[i]); }
+            }
+            if (ls.Count > n)
+            {
+                while (ls.Count > n)
+                {
+                    ls.RemoveAt(ls.Count - 1);
+                }
+            }
+            arr = new List<int>();
+            for (int j = ls.Count - 1; j >= 0; j--)
+            {
+                arr.Add(ls[j]);
+            }
+            return arr;
+        }
+    }
+}
---
+++ Задачки/Homework/Level B/TaskB6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    // Задача B6.
+    // Кол-во стингеров: ½🔷
+    //
+    // Написать функцию Reverser, которая меняет порядок букв в каждом слове заданного
+    // предложения на противоположный, порядок слов, при этом, должен сохраниться.
+    //
+    // Пример:
+    // Reverser("reverse letters") ==> "esrever srettel".
+    public static class TaskB6
+    {
+        public static string Reverser(string s)
+        {
+            string str = "";
+            string[] list = s.Split(' ');
+            for (int i = 0; i < list.Length; i++)
+            {
+                for (int j = list[i].Length - 1; j >= 0; j--)
+                {
+                    str += list[i][j];
+                }
+                if (i < list.Length - 1) { str += " "; }
+            }
+            if (str != "") { return str; }
+            return s;
+        }
+    }
+}
