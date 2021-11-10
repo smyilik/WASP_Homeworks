d7afdffcb420080e80b836f575fdd42ab54c4fe9
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-10 17:46:04 +0300

    Update Program.cs

--- Autopark/09_11_21 lesson/Program.cs
+++ Autopark/09_11_21 lesson/Program.cs
@@ -8,18 +8,25 @@ namespace _09_11_21_lesson
         protected string label;
         protected int power;
         protected int year;
-        public Car()
+        public Car(string label, int power, int year)
         {
-
+            this.label = label;
+            this.power = power;
+            this.year = year;
+        }
+        public virtual string ToString()
+        {
+            return "";
         }
     }
     public class PassengerCar : Car
     {
         protected int amountOfPassengers;
-        protected Dictionary<string, int> deteils = new Dictionary<string, int>();
-        public PassengerCar() : base()
+        protected Dictionary<string, int> deteils;
+        public PassengerCar(string label, int power, int year, int amountOfPassengers, Dictionary<string, int> deteils) : base(label, power, year)
         {
-
+            this.amountOfPassengers = amountOfPassengers;
+            this.deteils = deteils;
         }
         public void AddDeteil(string name, int year)
         {
@@ -31,24 +38,26 @@ namespace _09_11_21_lesson
         }
         public void PrintDeteils()
         {
-            foreach(KeyValuePair<string, int> h in deteils)
+            foreach (KeyValuePair<string, int> h in deteils)
             {
                 Console.WriteLine("Deteil " + h.Key + " Year " + h.Value);
             }
         }
-        public string ConvertToStr()
+        public override string ToString()
         {
-            return (label + " " + power + " " + year + " " + amountOfPassengers);
+            return label + " " + power + " " + year + " " + amountOfPassengers;
         }
     }
     public class Truck : Car
     {
         protected int maxmas;
         protected string drivername;
-        protected Dictionary<string, int> mas = new Dictionary<string, int>();
-        public Truck() : base()
+        protected Dictionary<string, int> mas;
+        public Truck(string label, int power, int year, int maxmas, string drivername, Dictionary<string, int> mas) : base(label, power, year)
         {
-
+            this.maxmas = maxmas;
+            this.drivername = drivername;
+            this.mas = mas;
         }
         public void NewDriver(string name)
         {
@@ -64,53 +73,55 @@ namespace _09_11_21_lesson
         }
         public void PrintMas()
         {
-            foreach(KeyValuePair<string, int> h in mas)
+            foreach (KeyValuePair<string, int> h in mas)
             {
                 Console.WriteLine("Product name: " + h.Key + " Product weight: " + h.Value);
             }
         }
-        public string ConvertToStr()
+        public override string ToString()
         {
-            return (label + " " + power + " " + year + " " + maxmas + " " + drivername);
+            return label + " " + power + " " + year + " " + maxmas + " " + drivername;
         }
-        class Autopark
+    }
+    class Autopark
+    {
+        protected string name;
+        protected List<PassengerCar> passengerCars;
+        protected List<Truck> trucks;
+        public Autopark(string name, List<PassengerCar> passengerCars, List<Truck> trucks)
         {
-            string name;
-            List<PassengerCar> passengerCars = new List<PassengerCar>();
-            List<Truck> trucks = new List<Truck>();
-            Autopark(string name)
+            this.name = name;
+            this.passengerCars = passengerCars;
+            this.trucks = trucks;
+        }
+        public string PrintInfo()
+        {
+            string str = "";
+            for (int i = 0; i < passengerCars.Count; i++)
             {
-                this.name = name;
+                str += passengerCars[i].ToString();
+                str += " ";
             }
-            public string PrintInfo()
+            for (int j = 0; j < trucks.Count; j++)
             {
-                string str = "";
-                for (int i = 0; i < passengerCars.Count; i++)
+                if (j == trucks.Count)
                 {
-                    str += passengerCars[i].ConvertToStr();
+                    str += trucks[j].ToString();
+                }
+                else
+                {
+                    str += trucks[j].ToString();
                     str += " ";
                 }
-                for (int j = 0; j < trucks.Count; j++)
-                {
-                    if (j == trucks.Count)
-                    {
-                        str += trucks[j].ConvertToStr();
-                    }
-                    else
-                    {
-                        str += trucks[j].ConvertToStr();
-                        str += " ";
-                    }
-                }
-                return str;
             }
+            return str;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
